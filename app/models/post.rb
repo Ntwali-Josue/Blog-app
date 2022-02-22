f614e31a7142212628ebcafb4d235a5403254d03
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-22 19:57:42 +0200

    Process data in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :users
+  has_many :comments, class_name: 'Comment'
+  has_many :likes, class_name: 'Like'
+
+  def update_comments_counter
+    commentscounter = Comment.where(post_id: id).count
+    update(CommentsCounter: commentscounter)
+  end
+
+  def update_likes_counter
+    likescounter = Like.where(post_id: id).count
+    update(LikesCounter: likescounter)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end

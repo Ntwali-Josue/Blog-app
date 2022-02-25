50177e9062fea2e3d0a1791df4e37c5c63262f0a
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-25 17:15:22 +0200

    Add flashes for actions on the posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,8 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @like = current_user.likes.new(post_id: @post.id)
+    @like = current_user.likes.new(post_id: @post.id])
+    flash[:notice] = 'You liked the post ❤️'
     respond_to do |format|
       format.html { redirect_to @post } if @post.save
     end
